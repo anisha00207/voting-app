a4c82f07212baae545657fff12e096dcd5465cbe
Anisha Joshi <90251007+anisha00207@users.noreply.github.com>
2024-05-03 11:30:55 +0530

    Add files via upload

---
+++ vote_app/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    
+    path('home/',views.home,name='home'),
+    path('logout/',views.logout,name='logout')
+
+  
+  
+  
+
+]
